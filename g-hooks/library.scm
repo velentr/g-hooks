469e30f336b3cef5c42f3859ffa59aa596b0f636
Brian Kubisiak <brian@kubisiak.com>
2023-09-05 17:04:24 -0700

    library: Add gitlint/commit-msg
    This runs gitlint as a commit-msg hooks to lint your git commit
    message. If the commit message violates the gitlint rules, the user
    has the option of re-editing it or accepting as-is.
    
    The 'program' macro cannot be used directly since we need to point
    stdin at /dev/tty in order to accept keyboard input to prompt the user
    on lint failure.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- g-hooks/library.scm
+++ g-hooks/library.scm
@@ -4,8 +4,11 @@
 
 (define-module (g-hooks library)
   #:use-module (gnu packages license)
+  #:use-module (gnu packages version-control)
+  #:use-module (guix gexp)
   #:use-module (g-hooks utils)
-  #:export (reuse-lint/pre-commit))
+  #:export (gitlint/commit-msg
+            reuse-lint/pre-commit))
 
 ;;; Commentary:
 ;;;
@@ -16,6 +19,16 @@
 ;;; g-hooks are only used for specific git hooks.
 ;;;
 ;;; Code:
+
+(define gitlint/commit-msg
+  #~(with-input-from-file "/dev/tty"
+      (lambda ()
+        #$(program gitlint
+                   "/bin/gitlint"
+                   "--staged"
+                   "--msg-filename"
+                   (cadr (command-line))
+                   "run-hook"))))
 
 (define reuse-lint/pre-commit
   (program reuse "/bin/reuse" "lint"))
