velentr/g-hooks

commit a95215f297567ef040150c4b6087c08ca05e0876

utils: Allow specifying package output for program macros
Both program and program* now allow (package output) syntax for the
package specification so that outputs other than "out" may be
used. This is useful for rustfmt, for example, which is in a different
output of the rust package.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/utils.scm
@@ -40,17 +40,25 @@ gexp."
       (unless (= rc 0)
         (exit rc))))
 
+(define-syntax program-path
+  (syntax-rules ()
+    ((program-path (package output) path)
+     ;; TODO: is it possible to set the output using file-append?
+     #~(string-append (ungexp package output) path))
+    ((program-path package path)
+     (file-append package path))))
+
 (define-syntax-rule (program package path args ...)
   "Run a program from PACKAGE, where the program is at PATH relative to the
 package root, with the given ARGS. This hook will exit on failure but not on
 success so it may be composed with other hooks."
-  (run (list #$(file-append package path) args ...)))
+  (run (list #$(program-path package path) args ...)))
 
 (define-syntax-rule (program* package path args ...)
   "Run a program from PACKAGE, where the program is at PATH relative to the
 package root, with the given ARGS, appending COMMAND-LINE to the args. This hook
 will exit on failure but not on success so it may be composed with other hooks."
-  (run (cons* #$(file-append package path)
+  (run (cons* (program-path package path)
               args ...
               (cdr (command-line)))))
 

commit 9a6c2b28325ef15f4fb846a9f5cc93cd81e86dad

utils: Fix program* macro
The program-path macro needs to be expanded at build-time, not during
run-time.

Fixes: commit a95215f ("utils: Allow specifying package output for program macros")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/utils.scm
@@ -59,7 +59,7 @@ success so it may be composed with other hooks."
   "Run a program from PACKAGE, where the program is at PATH relative to the
 package root, with the given ARGS, appending COMMAND-LINE to the args. This hook
 will exit on failure but not on success so it may be composed with other hooks."
-  (run (cons* (program-path package path)
+  (run (cons* #$(program-path package path)
               args ...
               (cdr (command-line)))))
 