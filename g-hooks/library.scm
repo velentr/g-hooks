velentr/g-hooks

commit eecf462ca161eb8281999adc7c5348d550cec41b

library: Add run-commit-hooks-on-applypatch
The pre-commit, commit-msg, and post-commit git hooks do not normally
run when applying a patch, but they often include linting that is
useful to run on patches from external contributors. The
run-commit-hooks-on-applypatch g-hook ensures that these git hooks are
run when applying a patch (e.g.) via 'git am'.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/library.scm
@@ -55,6 +55,7 @@
             git-lfs-hooks
             gitlint-run
             reuse-lint
+            run-commit-hooks-on-applypatch
             rustfmt-check
 
             ;; Deprecated g-expressions
@@ -340,6 +341,30 @@ filters for Git LFS."
   "Lint the project directory for compliance with version 3.0 of the REUSE
 Specification during the @code{pre-commit} hook."
   (pre-commit reuse-lint/pre-commit))
+
+(define (run-git-hook hook)
+  "Make a service extension for running the given git HOOK."
+  (lambda (config)
+    (list (program* git "/bin/git" "hook" "run" #$hook "--"))))
+
+(define g-hooks-commit-hooks-on-applypatch-service-type
+  (service-type
+   (name 'g-hooks-commit-hooks-on-applypatch)
+   (extensions
+    (list (service-extension g-hooks-pre-applypatch-service-type
+                             (run-git-hook "pre-commit"))
+          (service-extension g-hooks-post-applypatch-service-type
+                             (run-git-hook "post-commit"))
+          (service-extension g-hooks-applypatch-msg-service-type
+                             (run-git-hook "commit-msg"))))
+   (default-value '())
+   (description "Run the @code{commit-msg}, @code{pre-commit}, and
+@code{post-commit} hooks during @code{applypatch-msg}, @code{pre-applypatch},
+and @code{post-applypatch} respectively.")))
+
+(define (run-commit-hooks-on-applypatch)
+  "Run the normal commit hooks during applypatch."
+  (service g-hooks-commit-hooks-on-applypatch-service-type))
 
 (define rustfmt/pre-commit
   (for-each-staged-file

commit d74ae54a357088ef14564c40f13888ec2376158e

library: Ignore missing commit hooks during applypatch
When invoking 'git hook run', ignore missing hooks. It is not an error
to be missing these commit hooks; they should be a no-op when run
during applypatch.

Fixes: commit eecf462 ("library: Add run-commit-hooks-on-applypatch")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/library.scm
@@ -345,7 +345,13 @@ Specification during the @code{pre-commit} hook."
 (define (run-git-hook hook)
   "Make a service extension for running the given git HOOK."
   (lambda (config)
-    (list (program* git "/bin/git" "hook" "run" #$hook "--"))))
+    (list (program* git
+                    "/bin/git"
+                    "hook"
+                    "run"
+                    "--ignore-missing"
+                    #$hook
+                    "--"))))
 
 (define g-hooks-commit-hooks-on-applypatch-service-type
   (service-type