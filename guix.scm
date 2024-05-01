8afdc6c8633eaa1a4fc0afe98e95ffcd19c4499e
Brian Kubisiak <brian@kubisiak.com>
2024-04-30 17:00:30 -0700

    guix: Fix the package definition
    Ever since switching to building g-hooks as a guix extension, the
    package definition has (intentionally) been broken. Now that there is
    a Makefile, use gnu-build-system instead of guile-build-system to
    properly build and install the package.
    
    Fixes: eb6f204 ("core: Build g-hooks as a guix extension")
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- guix.scm
+++ guix.scm
@@ -7,7 +7,7 @@
              (gnu packages gnupg)
              (gnu packages package-management)
              (gnu packages version-control)
-             (guix build-system guile)
+             (guix build-system gnu)
              (guix gexp)
              ((guix licenses) #:prefix license:)
              (guix packages))
@@ -16,9 +16,10 @@
   (name "g-hooks")
   (version "0")
   (source (local-file "." "g-hooks" #:recursive? #t))
-  (build-system guile-build-system)
+  (build-system gnu-build-system)
   (arguments
    (list
+    #:tests? #f
     #:phases
     #~(modify-phases %standard-phases
         (add-after 'unpack 'patch-tool-inputs
@@ -26,38 +27,13 @@
             (substitute* "g-hooks/core.scm"
               (("define %git \"git\"")
                (string-append "define %git \"" (which "git") "\"")))))
-        (add-after 'unpack 'remove-unused-files
+        (replace 'configure
           (lambda _
-            (delete-file "guix.scm")))
-        (add-after 'install 'wrap-program
-          (lambda* (#:key inputs #:allow-other-keys)
-            (let* ((bin (string-append #$output "/bin"))
-                   (bytestructures (assoc-ref inputs "guile-bytestructures"))
-                   (gcrypt (assoc-ref inputs "guile-gcrypt"))
-                   (git (assoc-ref inputs "guile-git"))
-                   (guix (assoc-ref inputs "guix"))
-                   (version (target-guile-effective-version))
-                   (scm (string-append "/share/guile/site/" version))
-                   (go (string-append "/lib/guile/" version "/site-ccache")))
-              (install-file "scripts/g-hooks" bin)
-              (wrap-program (string-append bin "/g-hooks")
-                '("GUILE_AUTO_COMPILE" prefix ("0"))
-                ;; TODO: fix this nonsense
-                `("GUILE_LOAD_PATH" prefix
-                  (,(string-append #$output scm)
-                   ,(string-append bytestructures scm)
-                   ,(string-append gcrypt scm)
-                   ,(string-append git scm)
-                   ,(string-append guix scm)))
-                `("GUILE_LOAD_COMPILED_PATH" prefix
-                  (,(string-append #$output go)
-                   ,(string-append bytestructures go)
-                   ,(string-append gcrypt go)
-                   ,(string-append git go)
-                   ,(string-append guix go))))))))))
+            (setenv "GUILE_AUTO_COMPILE" "0")
+            (setenv "GUILE_FLAGS" "-L .")
+            (setenv "DESTDIR" #$output))))))
   (native-inputs (list guile-3.0))
-  (inputs
-   (list bash-minimal git guile-bytestructures guile-gcrypt guile-git guix))
+  (inputs (list git guix))
   (synopsis "Manage git hooks using guix")
   (description "g-hooks allows you to describe git hooks using guix’s
 g-expressions, then build and install them as a private profile under
