velentr/g-hooks

commit 57d28ba84da50fd067213a87fc961bde76604e84

core: Use guix's command-line parsing
Instead of using getopt-long, use guix's builtin command-line parsing,
which is based on SRFI-37. Using guix's parsing allows using the
standard build options and passing these options to the store
connection.

Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/core.scm
@@ -10,13 +10,15 @@
   #:use-module (guix derivations)
   #:use-module (guix monads)
   #:use-module (guix profiles)
+  #:use-module (guix scripts)
   #:use-module (guix scripts package)
   #:use-module (guix store)
   #:use-module (guix ui)
-  #:use-module (ice-9 getopt-long)
   #:use-module (ice-9 match)
   #:use-module (ice-9 popen)
   #:use-module (ice-9 rdelim)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-37)
   #:export (g-hooks
             g-hooks-main))
 
@@ -200,10 +202,6 @@ directory."
         (switch-to-generation* (force %g-hooks-profile) number)
         (error "cannot switch to generation:" generation-spec))))
 
-(define %main-options
-  '((version (single-char #\v) (value #f))
-    (help    (single-char #\h) (value #f))))
-
 (define %main-usage
   "\
 usage: g-hooks [-h | --help] [-v | --version] <command> [<args>]
@@ -221,27 +219,34 @@ the valid values for COMMAND are listed below:
   delete-generations  delete old g-hooks generations
 ")
 
+(define %main-options
+  (list
+   (option '(#\h "help") #f #f
+           (lambda _
+             (display %main-usage)
+             (exit 0)))
+   (option '(#\v "version") #f #f
+           (lambda _
+             (display "g-hooks version 0.6.0")
+             (newline)
+             (exit 0)))))
+
 (define %main-commands
   `(("reconfigure" . ,reconfigure)
     ("list-generations" . ,list-generations)
     ("delete-generations" . ,delete-generations)
     ("switch-generation" . ,switch-generation)))
 
+(define (parse-sub-command arg result)
+  "Parse ARG as a sub-command, adding it to RESULT."
+  (let ((command-processor (assoc-ref %main-commands arg)))
+    (if command-processor
+        (alist-cons 'action command-processor result)
+        (error "unrecognized command:" arg))))
+
 (define (g-hooks-main args)
-  (let ((options (getopt-long
-                  (cons "g-hooks" args)
-                  %main-options
-                  #:stop-at-first-non-option #t)))
-    (cond
-     ((option-ref options 'help #f)
-      (display %main-usage))
-     ((option-ref options 'version #f)
-      (display "g-hooks version 0.6.0")
-      (newline))
-     ((not (null? (option-ref options '() '())))
-      (let* ((arguments (option-ref options '() '()))
-             (command (car arguments))
-             (command-processor (assoc-ref %main-commands command)))
-        (if command-processor
-            (command-processor arguments)
-            (error "unrecognized command:" command)))))))
+  (let* ((opts (parse-command-line args %main-options '(())
+                                   #:argument-handler parse-sub-command))
+         (action (assoc-ref opts 'action)))
+    (when action
+      (action opts))))

commit a1d10d49a83e64fcc0c4865d3468cbbc48ecae64

core: Fix parsing switch-generation argument
Fix the parsing logic to allow an argument for switch-generation. This
could be done a bit more cleanly/robustly in the future, but this is
good enough for now.

Fixes: 57d28ba ("core: Use guix's command-line parsing")
Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

g-hooks/core.scm
@@ -113,8 +113,6 @@ directory."
 
 (define (reconfigure args)
   "Reconfigure .git/hooks/ based on the repository's g-hooks."
-  (unless (null? (cdr args))
-    (error "unrecognized arguments:" (string-join args)))
   (let ((g-hooks (load-config)))
     (run-with-store (open-connection)
       (mlet* %store-monad
@@ -179,22 +177,18 @@ directory."
 
 (define (delete-generations args)
   "Delete all generations other than the current generation."
-  (unless (null? (cdr args))
-    (error "unrecognized arguments:" (string-join args)))
   (with-store store
     (delete-matching-generations store (force %g-hooks-profile) #f)))
 
 (define (list-generations args)
   "Display all g-hooks generations in a human-readable format."
-  (unless (null? (cdr args))
-    (error "unrecognized arguments:" (string-join args)))
   (for-each print-generation (profile-generations (force %g-hooks-profile))))
 
 (define (switch-generation args)
   "Switch the g-hooks profile to the generation specified in ARGS."
-  (unless (= (length args) 2)
+  (unless (assoc-ref args 'argument)
     (error "switch-generation takes exactly one argument"))
-  (let* ((generation-spec (cadr args))
+  (let* ((generation-spec (assoc-ref args 'argument))
          (number (relative-generation-spec->number
                   (force %g-hooks-profile)
                   generation-spec)))
@@ -239,10 +233,13 @@ the valid values for COMMAND are listed below:
 
 (define (parse-sub-command arg result)
   "Parse ARG as a sub-command, adding it to RESULT."
-  (let ((command-processor (assoc-ref %main-commands arg)))
-    (if command-processor
-        (alist-cons 'action command-processor result)
-        (error "unrecognized command:" arg))))
+  (if (assoc-ref result 'action)
+      ;; we've already got an action, this is an argument
+      (alist-cons 'argument arg result)
+      (let ((command-processor (assoc-ref %main-commands arg)))
+        (if command-processor
+            (alist-cons 'action command-processor result)
+            (error "unrecognized command:" arg)))))
 
 (define (g-hooks-main args)
   (let* ((opts (parse-command-line args %main-options '(())